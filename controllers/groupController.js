62a64a8ffc14f2f480b8372c854353e1547116d8
ber <yanqinglou@outlook.com>
2023-03-06 17:20:15 -0800

    create group is done

--- controllers/groupController.js
+++ controllers/groupController.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { Game, User } = require("../models");
+const { Game, User, Group } = require("../models");
 const jwt = require("jsonwebtoken");
 
 //find all group
