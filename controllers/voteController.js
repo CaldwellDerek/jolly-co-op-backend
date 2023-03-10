d8a7ca442819b3451cf529314902a70a24147d30
ber <yanqinglou@outlook.com>
2023-03-10 10:51:11 -0800

    merge

--- controllers/voteController.js
+++ controllers/voteController.js
@@ -61,6 +61,7 @@ router.get("/user/:userid", async (req, res) => {
 //get vote of a game in a group
 router.get("/game/:gameid/group/:groupid", async (req, res) => {
   const token = req.headers?.authorization?.split(" ")[1];
+  console.log("TOKEN: "+token)
   // const tokenData = jwt.verify(token, process.env.JWT_SECRET);
   if (!token) {
     return res
